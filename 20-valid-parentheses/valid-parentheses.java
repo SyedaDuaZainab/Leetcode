0d3e552c60120d4ceba9014625e875fae229db4e
SyedaDuaZainab <sdua56963@gmail.com>
2026-05-08 20:59:21 +0500

    Time: 1 ms (99.88%) | Memory: 43 MB (92.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,49 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack=new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(ch=='(' || ch=='{' || ch=='['){
+//                 stack.push(ch);
+//             }else{
+//                 if(stack.isEmpty()) return false;
+//                 char top=stack.pop();
+//                 if(ch==')' && top!='(') return false;
+//                 if(ch=='}' && top!='{') return false;
+//                 if(ch==']' && top!='[') return false;
+//             } 
+           
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+//Its run time is 3ms and memory usage is 72.03% which is already good
+
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='['){
-                stack.push(ch);
-            }else{
-                if(stack.isEmpty()) return false;
-                char top=stack.pop();
-                if(ch==')' && top!='(') return false;
-                if(ch=='}' && top!='{') return false;
-                if(ch==']' && top!='[') return false;
+
+        char[] stack = new char[s.length()];
+        int top = -1;
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            // opening brackets
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack[++top] = ch;
             } 
-           
+            else {
+                if(top == -1) return false;
+
+                char prev = stack[top--];
+
+                if(ch == ')' && prev != '(') return false;
+                if(ch == '}' && prev != '{') return false;
+                if(ch == ']' && prev != '[') return false;
+            }
         }
-        return stack.isEmpty();
+
+        return top == -1;
     }
 }
+//
