d6ca06806a3215d48a0d46dc822d6270aa954b08
SyedaDuaZainab <sdua56963@gmail.com>
2026-05-09 20:49:47 +0500

    Time: 0 ms (100.00%) | Memory: 44.3 MB (56.05%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -8,21 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode dummy=new ListNode(0);
+//         ListNode current=dummy;
+//         while(list1!=null && list2!=null){
+//             if(list1.val<=list2.val){
+//                 current.next=list1;
+//                 list1=list1.next;
+//             }else{
+//                 current.next=list2;
+//                 list2=list2.next;
+//             }
+//             current=current.next;
+//         }
+//         current.next=(list1!=null) ? list1:list2;
+//         return dummy.next;
+//     }
+// }
+//Amazing, in this case iterative approach is better 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy=new ListNode(0);
-        ListNode current=dummy;
-        while(list1!=null && list2!=null){
-            if(list1.val<=list2.val){
-                current.next=list1;
-                list1=list1.next;
-            }else{
-                current.next=list2;
-                list2=list2.next;
-            }
-            current=current.next;
-        }
-        current.next=(list1!=null) ? list1:list2;
-        return dummy.next;
+if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+    // Recursive step: pick the smaller head
+    if (list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
+}
 }
