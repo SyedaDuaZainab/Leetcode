047a4a72cf95bc212ef52b55899b43dfd91a669f
SyedaDuaZainab <sdua56963@gmail.com>
2026-05-11 21:14:34 +0500

    Time: 0 ms (100.00%) | Memory: 43.3 MB (78.04%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -10,3 +10,13 @@ class Solution {
         return count;
     }
 }
+//[3,2,2,3] val=3
+//count=0 i=0 i<4 3!=3
+//count=0 i=1 1<4 2!=3 [2,2,2,3] count=1
+//count=1 i=2 2<4 2!=3 [2,2,2,3] count=2
+//count=2 i=3 3<4 3!=3 
+//count=2 i=4 4<4 X 
+//Output=2
+
+//Submission
+//100% Run time 
